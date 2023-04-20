5c7b5f0c2ee5aeec959e61dbf8b785116f4be2e3
qpwedev <russfdk@gmail.com>
2023-04-20 15:50:58 +0200

    everspace api class refactoring

--- src/api/everspace_center_api.py
+++ src/api/everspace_center_api.py
@@ -1,7 +1,8 @@
 import asyncio
+import math
 from typing import List
 import httpx
-from config import everspaceCenterConfig, ApiConfig, DEPTH
+from config import ApiConfig
 
 
 class EverspaceCenterApi:
@@ -13,37 +14,58 @@ class EverspaceCenterApi:
     def __init__(self, config: ApiConfig):
         self.config = config
 
-    async def get_transactions(self, address: str) -> List[dict]:
+    async def get_transactions(self, address: str, limit: int) -> List[dict]:
         """
         Get transactions for a given address asynchronously.
         :param address: The address for which to fetch the transactions.
+        :param limit: The maximum number of transactions to fetch.
         :return: A list of transactions if successful, an empty list otherwise.
         """
+
         headers = {
             'X-API-KEY': self.config.api_key,
         }
-        params = {
-            'address': address,
-            'limit': DEPTH,
-        }
 
-        for _ in range(self.config.retries):
-            try:
-                async with httpx.AsyncClient() as client:
-                    response = await client.get(
-                        'https://everspace.center/toncoin/getTransactions',
-                        params=params, headers=headers, timeout=self.config.timeout
-                    )
-                    response_json = response.json()
+        max_limit = 50
+        num_requests = math.ceil(limit / max_limit)
 
-                    if response.status_code == 200:
-                        return response_json
+        all_transactions = []
 
+        for _ in range(num_requests):
+            params = {
+                'address': address,
+                'limit': max_limit,
+            }
+
+            if all_transactions:
+                params['lt'] = all_transactions[-1]['lt']
+
+            for _ in range(self.config.retries):
+                try:
+                    async with httpx.AsyncClient() as client:
+                        response = await client.get(
+                            'https://everspace.center/toncoin/getTransactions',
+                            params=params, headers=headers, timeout=self.config.timeout
+                        )
+                        response_json = response.json()
+
+                        if response.status_code == 200:
+                            # Extract the "result" field containing the transactions
+                            all_transactions.extend(response_json)
+
+                            if len(response_json) < max_limit:
+                                break
+
+                        else:
+                            await asyncio.sleep(self.config.delay)
+                except httpx.HTTPError:
                     await asyncio.sleep(self.config.delay)
-            except httpx.HTTPError:
-                await asyncio.sleep(self.config.delay)
 
-        return []
+        if len(all_transactions) > limit:
+            # Return only the requested number of transactions
+            return all_transactions[:limit]
+        else:
+            return all_transactions
 
     async def get_balance(self, address: str) -> float:
         """
@@ -68,29 +90,10 @@ class EverspaceCenterApi:
                     response_json = response.json()
                     if response.status_code == 200:
                         balance = int(response_json['balance'])
-                        return balance
+                        return balance / 1e9
 
                     await asyncio.sleep(self.config.delay)
             except httpx.HTTPError:
                 await asyncio.sleep(self.config.delay)
 
         return 0
-
-
-async def main():
-    addresses = [
-        'EQDo8eYrFypI4cCZures4CiGsPXZyyHKR9-f6Vxly60h5lrh',
-        'EQDo8eYrFypI4cCZures4CiGsPXZyyHKR9-f6Vxly60h5lrh'
-    ]
-
-    api = EverspaceCenterApi(everspaceCenterConfig)
-
-    for address in addresses:
-        transactions = await api.get_transactions(address)
-        balance = await api.get_balance(address)
-        print(len(transactions), f'transactions for {address}')
-        print(f'Balance for {address}: {balance}')
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
